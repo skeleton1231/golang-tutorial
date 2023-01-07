85a7215f34ac0788e97d707f3bbe2ad94e338ca6
skeleton1231 <814010079@qq.com>
2023-01-07 23:44:46 +0800

    feat:use channel to share data as mutex

---
+++ channel/no_buffer/demo5/no_buffer_channel_mutex_005.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 不要通过共享内存来通信，而是通过通信来共享内存
+
+type counter struct {
+	c chan int
+	i int
+}
+
+func NewCounter() *counter {
+	cter := &counter{
+		c: make(chan int),
+	}
+	go func() {
+		for {
+			cter.i++
+			cter.c <- cter.i
+		}
+	}()
+	return cter
+}
+
+func (cter *counter) Increase() int {
+	return <-cter.c // receive int
+}
+
+func main() {
+	cter := NewCounter()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			v := cter.Increase()
+			fmt.Printf("goroutine-%d: current counter value is %d\n", i, v)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
